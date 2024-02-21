04f2aac796b0a0611c4b477fadd32ac86b79f236
Vanagandr111 <135647718+Vanagandr111@users.noreply.github.com>
2024-02-21 08:53:06 +0700

    1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = System.err::println;
+
+        Worker worker = new Worker(listener, errorListener);
+        worker.start();
+    }
+
+
+}
+
+
